c43cc3a4d35bbf09a5aedc93d75a4f1eb399f809
Gadu <alexanderudag@gmail.com>
2022-06-07 15:55:07 +0800

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      floQastGreen: {
+        main: '#8bc540',
+        contrastText: '#fff',
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="titleContainer">
+      <h2><img src={require('./res/floqast.png')} alt="floqast logo"/><span className="darkGray">The</span></h2>
+      <h1 className='green' style={{fontSize: '60px'}}>Fastest, Most Accurate</h1>
+      <h1 className='darkGray'>Way to Close Your Books!</h1>
     </div>
+
+      <center>
+        <div className="formContainer">
+                <Grid container spacing={0}>
+                    <Grid item xs={12} lg={4}>
+                      <div className="formTitle">
+                        <h1>SCHEDULE A DEMO</h1>
+                        <h4>Learn more about FloQast.</h4> 
+                      </div>
+                    </Grid>
+                    <Grid item xs={12} lg={8} style={{paddingRight: "15px", paddingLeft: "15px"}}>
+                      <h2 className="formTop"><span className="gray">Learn How FloQast Can</span> <span className="green">Improve Your Month-End</span></h2>
+                      <Grid container spacing={2} style={{paddingRight: "15px", paddingLeft: "15px", paddingBottom: "15px"}}>
+                        <ThemeProvider theme={theme}>
+                          <Grid item xs={12} md={4}>
+                            <TextField id="outlined-basic" label="First Name" variant="outlined" size='small' color="floQastGreen" required fullWidth/>
+                          </Grid>
+                          <Grid item xs={12} md={4}>
+                            <TextField id="outlined-basic" label="Email" variant="outlined" size='small' color="floQastGreen" required fullWidth/>
+                          </Grid>
+                          <Grid item xs={12} md={4}>
+                          <Button variant="contained" color="floQastGreen" fullWidth>SCHEDULE NOW</Button>
+                          </Grid>
+                        </ThemeProvider>
+                      </Grid>
+                    </Grid>
+                </Grid>
+          </div>
+        </center>
+    </>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { pink, red } from '@mui/material/colors';
+
+export default makeStyles(() => ({
+    formContainer:{
+        position: 'absolute',
+        bottom: '10vh',
+        left: '50%',
+        right: '50%'
+    }
+}));
